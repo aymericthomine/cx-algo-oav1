e6ce84d8885b8b6deadd0d27c6164d634d5a08e4
Aymeric <aymeric@MacBook-Pro-de-Aymeric.local>
2020-10-16 21:03:12 +0200

    Add e03.js

--- e03.js
+++ e03.js
@@ -1,20 +1,16 @@
 function marsos(str) {
-    const sosCount = str.length / 3
-    const originalMessage = 'SOS'.repeat(sosCount)
-  
-    let errorCount = 0
-  
-    for (let i = 0; i < str.length; i++) {
-      if (str[i] !== originalMessage[i]) {
-        errorCount++
-      }
-    }
-  
-    return errorCount
-  }
-  
-  console.log(marsos('SOFSOSSISSOW'))
+  const sosCount = str.length / 3
+  const originalMessage = 'SOS'.repeat(sosCount)
 
+  let errorCount = 0
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== originalMessage[i]) {
+      errorCount++
+    }
+  }
+
+  return errorCount
 }
 
-console.log(marsus('SOFSOSSISSOW'))
+console.log(marsos('SOFSOSSISSOW'))
