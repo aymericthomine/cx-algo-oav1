f2d0fc80e09a31142ae9b531d6745a432fa0986d
Aymeric <aymeric@MacBook-Pro-de-Aymeric.local>
2020-10-16 11:08:42 +0200

    Add e01.js

--- e01.js
+++ e01.js
@@ -2,14 +2,18 @@ function howManyCamelCase(str = '') {
     let sentence = ''
     let count = 0
   
+    if (str.charCodeAt(0) >= 97 && str.charCodeAt(0) <= 122) {
+        count++;
+    }
+
     for (let i = 0; i < str.length; i++) {         
         if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
             count++;
         } 
-
-        return count
-
     }  
+
+    return count
+
 }
 
-console.log(howManyCamelCase('testTestTest'))
+console.log(howManyCamelCase('iLoveTennis'))
